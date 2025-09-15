1fb7b36f6dc9ac285a2ec3e389c9023ff418e672
devjohn42 <devjohncb@gmail.com>
2025-09-15 18:13:02 -0300

    :closed_lock_with_key: security(cors): restricts allowed origins for frontend

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import { confirmPresence } from './http/confirm-presence.js'
 import { createTrip } from './http/create-trips.js'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: '*'
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
+app.register(confirmPresence)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('✅ HTTP SERVER RUNNING ✅')
